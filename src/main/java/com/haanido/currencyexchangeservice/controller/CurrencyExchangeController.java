58032eb116477ad16e3e5c69c44db91da19cdee0
IdodeHaan <ido.de.haan@live.nl>
2023-06-20 15:48:47 +0200

    first commit

---
+++ src/main/java/com/haanido/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,28 @@
+package com.haanido.currencyexchangeservice.controller;
+
+import com.haanido.currencyexchangeservice.model.CurrencyExchange;
+import com.haanido.currencyexchangeservice.repo.CurrencyExchangeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CurrencyExchangeController {
+
+    private final Environment environment;
+    private final CurrencyExchangeRepository repository;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
+        }
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
+    }
+}
